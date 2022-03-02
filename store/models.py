243d5368a515d8588e1db4370c4e8ffc77fc4388
stephanieraymos <stephanieraymos@gmail.com>
2022-03-01 19:11:46 -0800

    membership field for customer model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
+from email.policy import default
 from enum import unique
 from django.db import models
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -8,9 +10,20 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
 
+
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=100)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
